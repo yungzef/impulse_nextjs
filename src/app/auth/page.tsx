3ac25cc805468774a5bffd644f641e2cb1496425
serhiifilatov <zefippppp@gmail.com>
2025-06-04 20:58:34 +0300

    ma13

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,10 @@
-import ClientWrapper from "./ClientWrapper"; // без dynamic
+import dynamic from "next/dynamic";
+import type { FC } from "react";
+
+// ⬇️ добавляем точную аннотацию и каст
+const ClientWrapper = dynamic(() => import("./ClientWrapper") as Promise<{ default: FC }>, {
+    ssr: false,
+});
 
 export default function Page() {
     return <ClientWrapper />;
