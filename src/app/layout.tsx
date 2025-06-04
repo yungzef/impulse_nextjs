f0c377337275f39c28459eb43a4d8f1fc37dccb1
serhiifilatov <zefippppp@gmail.com>
2025-06-04 15:03:01 +0300

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,8 @@
 "use client";
 
-import {Exo_2} from 'next/font/google';
-import {Inter} from 'next/font/google';
-import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
 import {SessionProvider} from "next-auth/react";
-import SubscriptionDialog from "@/app/components/SubscriptionDialog";
-
-export const exo2 = Exo_2({
-    variable: '--font-exo2',
-    subsets: ['latin', 'cyrillic'], // Добавьте 'cyrillic' для поддержки кириллицы
-    display: 'swap',
-});
-
-export const inter = Inter({
-    subsets: ['latin'],
-    variable: '--font-inter',
-});
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export default function RootLayout({
                                        children,
