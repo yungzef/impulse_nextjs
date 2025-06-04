a80048d44c21f2b5b41a777eacd41cf35c4f6362
serhiifilatov <zefippppp@gmail.com>
2025-06-04 18:56:15 +0300

    ma5

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,12 @@
 import AuthGuard from "@/app/components/AuthGuard";
 import SubscriptionDialog from "@/app/components/SubscriptionDialog";
+import {Providers} from "@/app/providers";
+import {SessionProvider} from "next-auth/react";
 
 export default function DashboardLayout({
                                             children,
                                         }: {
     children: React.ReactNode;
 }) {
-    return <AuthGuard>{children}<SubscriptionDialog/></AuthGuard>;
+    return <SessionProvider><Providers><AuthGuard>{children}<SubscriptionDialog/></AuthGuard></Providers></SessionProvider>;
 }
