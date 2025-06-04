f36ee5026f3bd9fb9a7906bd089d7c85cc53300c
serhiifilatov <zefippppp@gmail.com>
2025-06-04 19:18:10 +0300

    ma9

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,36 +1,7 @@
-"use client";
 export const dynamic = 'force-dynamic';
-import { useEffect, useState } from "react";
-import ModeCards from "@/app/dashboard/components/ModeCards";
-import Navbar from "@/app/dashboard/components/Navbar";
-import HeroSection from "@/app/dashboard/components/HeroSection";
-import UserProgress from "@/app/dashboard/components/UserProgress";
-import UserDetailedStats from "@/app/dashboard/components/UserDetailedStats";
-import Footer from "@/app/components/FooterSection";
 
-export default function DashboardPage() {
-    const [userName, setUserName] = useState("");
-    const [userId, setUserId] = useState("");
+import ClientWrapper from './ClientWrapper';
 
-    useEffect(() => {
-        const name = localStorage.getItem("user_name");
-        const id = localStorage.getItem("user_id");
-        if (name) setUserName(name);
-        if (id) setUserId(id);
-    }, []);
-
-    return (
-        <div className="min-h-screen bg-base-100 flex flex-col">
-            <Navbar/>
-            <HeroSection name={userName} />
-
-            {/* ⬇️ Только когда userId есть */}
-            {userId && <UserProgress userId={userId} />}
-
-            <ModeCards />
-
-            <UserDetailedStats userId={userId} />
-            <Footer />
-        </div>
-    );
+export default function Page() {
+    return <ClientWrapper />;
 }
